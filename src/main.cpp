1232972068525020d37d7874f78d7b0220c64d11
Harrison Plowes <s240881@students.aie.edu.au>
2025-02-13 12:20:28 +1100

    working on window initialisation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #ifndef UNICODE
 #define UNICODE
-#endif 
+#endif
+
+#if _DEBUG
+// enable mem leak detection
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+#endif
 
 #include <cstdlib>
 #include <windows.h>
@@ -8,57 +14,39 @@
 #include <chrono>
 #include <atomic>
 
+struct StateInfo {
+
+};
+
 std::atomic_bool g_running{ true };
 HWND g_hwnd = nullptr;
 
 LRESULT __stdcall WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+void WinThread(HINSTANCE hInstance, int nCmdShow, StateInfo* pState);
 
-void WinThread(HINSTANCE hInstance, int nCmdShow) {
-    const wchar_t* CLASS_NAME = L"WindOwO";
+int __stdcall wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ PWSTR, _In_ int nCmdShow) {
+#ifdef _DEBUG
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
+    StateInfo* pState = new StateInfo();
+    if (pState == NULL) return 0;
 
-    RegisterClassW(&wc);
-
-    g_hwnd = CreateWindowExW(0, CLASS_NAME, L"WindOwO", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
-        CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, NULL);
-
-    if (g_hwnd == NULL) {
-        g_running = false;
-        return;
-    }
-
-    ShowWindow(g_hwnd, nCmdShow);
-
-    MSG msg = {};
-    while (GetMessageW(&msg, 0, 0, 0) > 0) {
-        TranslateMessage(&msg);
-        DispatchMessageW(&msg);
-    }
-
-    g_running = false;
-}
-
-int __stdcall wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
-    
-    std::thread winThread(WinThread, hInstance, nCmdShow);
+    std::thread winThread(WinThread, hInstance, nCmdShow, pState);
 
     int rectX = 50;
     int rectY = 50;
     const int rectW = 50;
     const int rectH = 50;
-    const int rectS = 10;
+    const int rectSpeed = 10;
 
     while (g_running) {
         
         // input handling
-        if (GetAsyncKeyState(VK_LEFT) & 0x8000) rectX -= rectS;
-        if (GetAsyncKeyState(VK_RIGHT) & 0x8000) rectX += rectS;
-        if (GetAsyncKeyState(VK_UP) & 0x8000) rectY -= rectS;
-        if (GetAsyncKeyState(VK_DOWN) & 0x8000) rectY += rectS;
+        if (GetAsyncKeyState(VK_LEFT) & 0x8000) rectX -= rectSpeed;
+        if (GetAsyncKeyState(VK_RIGHT) & 0x8000) rectX += rectSpeed;
+        if (GetAsyncKeyState(VK_UP) & 0x8000) rectY -= rectSpeed;
+        if (GetAsyncKeyState(VK_DOWN) & 0x8000) rectY += rectSpeed;
         
         // drawing
         if (g_hwnd) {
@@ -77,11 +65,43 @@ int __stdcall wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow) {
             }
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(3));
+        std::this_thread::sleep_for(std::chrono::milliseconds(8));
     }
 
+    // cleanup
     winThread.join();
+    delete pState;
     return 0;
+}
+
+void WinThread(HINSTANCE hInstance, int nCmdShow, StateInfo* pState) {
+    const wchar_t* CLASS_NAME = L"WindOwO";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClassW(&wc);
+
+    g_hwnd = CreateWindowExW(0, CLASS_NAME, L"WindOwO", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+        CW_USEDEFAULT, 800, 600, NULL, NULL, hInstance, pState);
+
+    if (g_hwnd == NULL) {
+        g_running = false;
+        return;
+    }
+
+    ShowWindow(g_hwnd, nCmdShow);
+
+    MSG msg = {};
+    while (GetMessageW(&msg, 0, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessageW(&msg);
+    }
+
+    g_running = false;
+    return;
 }
 
 LRESULT __stdcall WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
